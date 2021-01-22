2373066414e8de17b8483082dda8702e3c19a797
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-22 15:40:36 -0500

    Complex Calculator

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int getMax(int num1, int num2, int num3){
-    int result;
-
-    if(num1 >= num2 && num1 >= num3){
-        result = num1;
-    } else if(num2 >= num1 && num2 >= num3){
-        result = num2;
-    } else {
-        result = num3;
-    }
-
-    return result;
-}
-
 int main() {
 
-    cout << getMax(2,5,11);
+    int num1, num2;
+    char op;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator: ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+    int result;
 
+    if(op == '+'){
+        result = num1 + num2;
+    } else if (op == '-'){
+        result = num1 - num2;
+    } else if (op == '/'){
+        result = num1 / num2;
+    } else if (op == '*'){
+        result = num1 * num2;
+    } else {
+        cout << "Invalid Operator";
+    }
+    cout << result;
     return 0;
 }
