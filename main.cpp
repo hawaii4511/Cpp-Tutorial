5c5fb04e2bf863be86bc8d7d1209bbe11d8b44cf
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-21 19:49:24 -0500

    Return Statement

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
-void sayHi(string name, int age){
-    cout << "Hello " << name << " you are " << age << endl;
+double cube(double num){
+    double result = num*num*num;
+    return result;
 }
 
 int main() {
 
-    cout << "Top" << endl;
-    sayHi("Hannah", 50);
-    sayHi("Mae", 30);
-    sayHi("Dan", 10);
-    cout << "Bottom";
+    double answer = cube(5.0);
+    cout << answer;
 
     return 0;
 }
