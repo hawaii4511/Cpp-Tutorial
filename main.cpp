326ecec4faa3c3a0fa2b7fb3960058f7056d0107
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-22 15:48:06 -0500

    Switch Statements

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 
+
+string getDayOfWeek(int dayNum){
+    string dayName;
+
+    switch(dayNum){
+    case 0:
+        dayName = "Sunday";
+        break;
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    default:
+        dayName = "Invalid Day Number";
+        break;
+    }
+
+    return dayName;
+}
+
 int main() {
 
-    int num1, num2;
-    char op;
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter operator: ";
-    cin >> op;
-    cout << "Enter second number: ";
-    cin >> num2;
-    int result;
+    cout << getDayOfWeek(5);
 
-    if(op == '+'){
-        result = num1 + num2;
-    } else if (op == '-'){
-        result = num1 - num2;
-    } else if (op == '/'){
-        result = num1 / num2;
-    } else if (op == '*'){
-        result = num1 * num2;
-    } else {
-        cout << "Invalid Operator";
-    }
-    cout << result;
     return 0;
 }
