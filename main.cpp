1c00e227eceffe3351a0298247c08ad7b075c526
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-22 15:33:05 -0500

    If statements continued

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int getMax(int num1, int num2, int num3){
+    int result;
 
+    if(num1 >= num2 && num1 >= num3){
+        result = num1;
+    } else if(num2 >= num1 && num2 >= num3){
+        result = num2;
+    } else {
+        result = num3;
+    }
+
+    return result;
+}
 
 int main() {
 
-    bool isMale = false;
-    bool isTall = false;
-    if(isMale && isTall) {
-        cout << "you are a tall male";
-    } else if(isMale && !isTall) {
-        cout << "You are a short male";
-    } else if (!isMale && isTall) {
-        cout << "You are a tall female";
-    } else {
-        cout << "You are a short female";
-    }
+    cout << getMax(2,5,11);
 
     return 0;
 }
