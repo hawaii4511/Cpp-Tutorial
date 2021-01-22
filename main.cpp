cd76ba79d09a458333b895a56a180037c4b308b5
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-22 15:25:06 -0500

    If Statements

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-double cube(double num){
-    double result = num*num*num;
-    return result;
-}
+
 
 int main() {
 
-    double answer = cube(5.0);
-    cout << answer;
+    bool isMale = false;
+    bool isTall = false;
+    if(isMale && isTall) {
+        cout << "you are a tall male";
+    } else if(isMale && !isTall) {
+        cout << "You are a short male";
+    } else if (!isMale && isTall) {
+        cout << "You are a tall female";
+    } else {
+        cout << "You are a short female";
+    }
 
     return 0;
 }
