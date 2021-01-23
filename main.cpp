935883dc161bdbbefdc5af8aca658ecf41bcbeb6
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-22 19:03:49 -0500

    Build a guessing game

--- main.cpp
+++ main.cpp
@@ -5,11 +5,28 @@ using namespace std;
 
 int main() {
 
-    int index = 1;
-    do{
-        cout << index << endl;
-        index++;
-    } while(index <=5);
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
 
+    while(secretNum != guess && !outOfGuesses){
+        if(guessCount<guessLimit){
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
+
+    }
+
+    if(outOfGuesses){
+        cout << "You lose!";
+    } else {
+        cout << "You win!";
+    }
+    
     return 0;
 }
