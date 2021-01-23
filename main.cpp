e836d1dfcfdcdc9394c7e367c0a8ce4ee539aa0c
Hannana <71953988+hawaii4511@users.noreply.github.com>
2021-01-23 17:54:48 -0500

    Comments

--- main.cpp
+++ main.cpp
@@ -4,18 +4,8 @@ using namespace std;
 
 int main() {
 
-    int numberGrid[3][2] = {
-        {1,2},
-        {3,4},
-        {5,6}
-    };
-
-    for(int i = 0; i<3; i++){
-        for(int j=0; j<2; j++){
-            cout << numberGrid[i][j];
-        }
-        cout << endl;
-    }
+    // Comment
+    cout << "Comments are fun";
  
     return 0;
 }
